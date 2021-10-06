5c4e8193740165a91a35c9732f5cdb4268ce9eac
martin jakubik <martin.jakubik@gmail.com>
2021-10-06 08:46:05 +0200

    mockdata script

---
+++ mockdata.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+KEEPER_DIR_NAME=~/.fakekeeper
+
+echo -e "gimpy@playhouse.com\nJumpySpider4" > $KEEPER_DIR_NAME/bankaccount
+echo -e "ladybug\nYellowDonkey" > $KEEPER_DIR_NAME/insurance
+echo -e "pokaroo\nMissedHimAgain" > $KEEPER_DIR_NAME/groceries
+echo -e "alfred@wayne.com\nBatmanRules100" > $KEEPER_DIR_NAME/retirement
+echo -e "marty@mcfly.com\n1.21Gigawatts" > $KEEPER_DIR_NAME/energy
+echo -e "j.alfred.prufrock\n04CruellestMonth" > $KEEPER_DIR_NAME/heating
