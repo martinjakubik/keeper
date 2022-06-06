77f389954f6865ceb394b8f5a149ec7d324ac844
martin jakubik <martin.jakubik@gmail.com>
2022-06-06 09:45:58 +0200

    added more mock files

--- mockdata.sh
+++ mockdata.sh
@@ -8,6 +8,7 @@ if [[ ! -e $KEEPER_DIR_NAME ]] ; then
 fi
 
 echo -e "gimpy@playhouse.com\nJumpySpider4" > $KEEPER_DIR_NAME/bankaccount
+echo -e "mamasmurf\nAzriel3" > $KEEPER_DIR_NAME/carpets
 echo -e "ladybug\nYellowDonkey" > $KEEPER_DIR_NAME/insurance
 echo -e "pokaroo\nMissedHimAgain" > $KEEPER_DIR_NAME/groceries
 echo -e "alfred@wayne.com\nBatmanRules100" > $KEEPER_DIR_NAME/retirement
@@ -16,4 +17,8 @@ echo -e "j.alfred.prufrock\n04CruellestMonth" > $KEEPER_DIR_NAME/heating
 
 echo -e "__long_content__\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\nabcdefghijklmnopqrstuvwxyz\n" > $KEEPER_DIR_NAME/carwash
 
-gpg -c -a --cipher-algo AES256 $KEEPER_DIR_NAME/insurance
+for counter in {1..50} ; do
+    echo -e "userid"$counter\n"password"$counter > $KEEPER_DIR_NAME/zfile$counter
+done
+
+gpg -c -a --cipher-algo AES256 $KEEPER_DIR_NAME/carpets
