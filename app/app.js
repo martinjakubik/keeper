c575ca8c1738e85a1614cc86579efcde3bee6231
martin jakubik <martin.jakubik@gmail.com>
2022-06-06 09:21:50 +0200

    tidy

--- app/app.js
+++ app/app.js
@@ -1,7 +1,6 @@
 const oOpenPgp = require('openpgp');
 const { readFile } = require('fs/promises');
 const { ipcRenderer } = require('electron');
-const { BrowserWindow } = require('electron');
 
 const sDefaultKeeperDirectory = '/Users/martin/.fakekeeper';
 
